413aed69bcbee8d0dc2dd0c758bd74134b03693e
SAURABH <sgupta8mz@gmail.com>
2023-12-27 20:26:53 +0530

    first commit

---
+++ first-project/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import Product from './Product'
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1>Hello There</h1>
+      <h1>Hello</h1>
+
+      <Product/>
+
+      <Product/>
+      <Product/>
+      <Product/>
+
+    </div>
+  )
+  
+}
+
+export default App
---
+++ first-project/src/Product.jsx
@@ -0,0 +1,10 @@
+function Product()
+{
+    return (
+        <h1>I am a product component</h1>
+    )
+}
+
+
+export default Product;
+
